0409978da6c8d1e0b4b916499db1eebbf17d7754
blackweeed <blackweeeed@gmail.com>
2023-03-10 19:38:50 +0100

    product page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Navbar from "./components/Navbar";
-import Slider from "./components/Slider";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       {/* <Home /> */}
+      <Product />
     </div>
   );
 }
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,5 +1,25 @@
+import data from "../data/storeData.js";
+
+const item = data.data[2];
+
 function Product() {
-  return <div>Product</div>;
+  return (
+    <div>
+      <div className="slider"></div>
+      <div className="px-[1rem]">
+        <h2 className="text-[1.3125rem] text-[color:var(--cx-color-primary)]">
+          {item.name} - Black
+        </h2>
+        <h3 className="text-[.875rem] max-w-[90%] mb-2">{item.description}</h3>
+        <p className="text-[1.3125rem]">US${item.price}</p>
+        <ul className="text-[#888] list-disc ml-5 text-[.875rem] mt-[2rem]">
+          <li>Ultra-lightweight Design</li>
+          <li>Refined Ergonomic Form</li>
+          <li>Razer™ Focus Pro 30K Optical Sensor</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
